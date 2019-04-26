1cf77b1f3516cb67f8ba60c9b7becec2352122ec
Brian Yu <brianyu28@gmail.com>
2019-04-25 20:25:53 -0400

    Updates

---
+++ precache-manifest.be2c241564f0c6952af6a3d34c8652f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e964803fb68b13efc637a1b02cf34cc",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "be3c7fe094e310c7d63b",
+    "url": "/jeopardy/static/css/main.3d203869.chunk.css"
+  },
+  {
+    "revision": "b1da41801ec13e71f5fa",
+    "url": "/jeopardy/static/js/2.c7d4d1a1.chunk.js"
+  },
+  {
+    "revision": "be3c7fe094e310c7d63b",
+    "url": "/jeopardy/static/js/main.fb3f81ee.chunk.js"
+  },
+  {
+    "revision": "62ac5bd5a1faa6c116fa",
+    "url": "/jeopardy/static/js/runtime~main.f29172a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ebd7a818b93ba909ad815faa6ef641c.js"
+  "/jeopardy/precache-manifest.be2c241564f0c6952af6a3d34c8652f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jeopardy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
